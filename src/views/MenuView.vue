0ec2c408652c04ff0fabc8c6083b3e0d5a9812dc
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-11-19 14:42:55 +0900

    Basic Menus page, pre Filter

---
+++ src/views/MenuView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menu-container">
+    <div class="title">Menus</div>
+    <div class="menu-text">
+      <p>
+        Making good food requires fresh ingredients, proper techniques, and a
+        passion for cooking. Always start with high-quality ingredients and take
+        your time to prepare them properly. Experiment with different flavors
+        and don't be afraid to try new recipes. Happy cooking!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.menu-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: 'Unna', serif;
+}
+.title {
+  font-family: 'Unna', serif;
+  font-size: 6em;
+  font-weight: 100;
+  font-style: italic;
+}
+</style>
