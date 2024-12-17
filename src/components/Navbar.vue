3e32e6e94df8d2f97987c5919b9cab4872fb56bf
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-12-17 15:29:33 +0900

    Fixed Navbar Styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <router-link to="/"><div class="logo">Gavi</div></router-link>
+    <router-link class="logo-container" to="/"
+      ><div class="logo">Gavi</div></router-link
+    >
     <ul class="nav-links">
       <li>
         <router-link to="/reserve" active-class="active-link"
@@ -80,6 +82,14 @@ export default {
   font-family: Lora, serif;
   padding: 1rem;
 }
+.logo-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+
 .logo {
   font-family: 'CustomFont', sans-serif;
   font-size: 6rem;
@@ -96,7 +106,6 @@ export default {
   padding: 10% 0%;
   font-size: 1rem;
   font-weight: bold;
-  border-top: 1px solid black;
 }
 .nav-links li {
   text-align: center;
