6abe4d9dc3bb72b15201644b10838f1663f6467d
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-01-02 13:24:42 +0900

    Fixed Menu Button appearing in non mobile

--- src/App.vue
+++ src/App.vue
@@ -80,63 +80,62 @@ export default {
     height: auto;
     width: 100%;
   }
-}
+  /* Mobile Menu Toggle */
+  .menu-icon {
+    position: fixed;
+    top: 30px;
+    right: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 30px;
+    height: 30px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    z-index: 20;
+  }
 
-/* Mobile Menu Toggle */
-.menu-icon {
-  position: fixed;
-  top: 30px;
-  right: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 30px;
-  height: 30px;
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 0;
-  z-index: 20;
-}
+  .menu-icon:focus {
+    outline: none;
+  }
 
-.menu-icon:focus {
-  outline: none;
-}
+  .menu-icon div {
+    width: 30px;
+    height: 3px;
+    background: var(--primary);
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+  }
 
-.menu-icon div {
-  width: 30px;
-  height: 3px;
-  background: var(--primary);
-  border-radius: 10px;
-  transition: all 0.3s linear;
-  position: relative;
-  transform-origin: 1px;
-}
+  .menu-icon div:nth-child(1) {
+    transform: rotate(0);
+  }
 
-.menu-icon div:nth-child(1) {
-  transform: rotate(0);
-}
+  .menu-icon div:nth-child(2) {
+    opacity: 1;
+    transform: translateX(0);
+  }
 
-.menu-icon div:nth-child(2) {
-  opacity: 1;
-  transform: translateX(0);
-}
+  .menu-icon div:nth-child(3) {
+    transform: rotate(0);
+  }
 
-.menu-icon div:nth-child(3) {
-  transform: rotate(0);
-}
+  .menu-icon.open div:nth-child(1) {
+    transform: rotate(45deg);
+  }
 
-.menu-icon.open div:nth-child(1) {
-  transform: rotate(45deg);
-}
+  .menu-icon.open div:nth-child(2) {
+    opacity: 0;
+    transform: translateX(0px);
+    transition: opacity 0.1s linear;
+  }
 
-.menu-icon.open div:nth-child(2) {
-  opacity: 0;
-  transform: translateX(0px);
-  transition: opacity 0.1s linear;
-}
-
-.menu-icon.open div:nth-child(3) {
-  transform: rotate(-45deg);
+  .menu-icon.open div:nth-child(3) {
+    transform: rotate(-45deg);
+  }
 }
 </style>
